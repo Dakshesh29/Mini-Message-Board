9dd91b3cc99bc8a9486ac8218cb64a7b1e4002a2
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-08-06 12:33:04 +0530

    basic app.js setup finish

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ import express from "express";
 import path from "path";
 import indexRouter from "./routes/index";
 
-const app = express;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
