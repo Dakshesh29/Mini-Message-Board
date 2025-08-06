23d05f13128bd52b6b3215481a82008343f879a0
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-08-06 12:45:05 +0530

    finish index.js setup

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+const router = express.Router;
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages });
+});
+
+router.get("/new", (req, res) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const { messagesText, messagesUser } = req.body;
+  messages.push({
+    text: messagesText,
+    user: messagesUser,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
+router.get("/message/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const message = messages[id];
+  if (!message) return res.status(404).send("Message not found");
+  res.render("message", { message });
+});
+
+module.exports = router;
